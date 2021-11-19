9d44da2ab4398feee34172c7911a6815fca625c5
JackMechem <mechemjack@gmail.com>
2021-11-18 21:58:02 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import logo from './logo.svg';
 import './App.css';
+import colors from './style/colors';
+import styled from 'styled-components';
+
+const Background = styled.div`
+  background-color: ${colors.background};
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  padding: 10px
+`
+const Title = styled.text`
+  color: ${colors.title};
+  font-weight: 500;
+  font-size: large;
+`
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Background>
+      <Title>Jack Mechem</Title>
+    </Background>
   );
 }
 
